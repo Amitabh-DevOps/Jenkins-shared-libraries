c1d6a19f722bf543a80aecf6c33f47b0933e7c4c
Amitabh soni <amitabhdevops2024@gmail.com>
2025-03-25 13:10:15 +0530

    Update dockerScanTrivy.groovy

--- vars/dockerScanTrivy.groovy
+++ vars/dockerScanTrivy.groovy
@@ -3,8 +3,9 @@
 //     sh "trivy fs ."
 // }
 
-def call(String reportFile = "table-report.html") {
+def call(String image, String tag, String reportFile = "table-report.html") {
     script {
+        echo "Scanning Docker image ${image}:${tag} with Trivy..."
         sh "trivy fs --format table -o ${reportFile} ."
         archiveArtifacts artifacts: reportFile, fingerprint: true
     }
