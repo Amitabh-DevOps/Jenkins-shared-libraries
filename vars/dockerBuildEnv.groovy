3500b83121af4f9aae59e9f596b1b2cf5a1bbbfe
Amitabh soni <amitabhdevops2024@gmail.com>
2025-04-02 14:14:59 +0530

    Update dockerBuildEnv.groovy

--- vars/dockerBuildEnv.groovy
+++ vars/dockerBuildEnv.groovy
@@ -1,5 +1,9 @@
 // vars/dockerBuildEnv.groovy
+// vars/dockerbuild.groovy
+
 def call(String imageName, String tag, String buildArgs = "", String apiKey) {
-    // Use the provided apiKey as an environment variable for the build
-    sh "NEXT_PUBLIC_API_KEY=${apiKey} docker build ${buildArgs} -t ${imageName}:${tag} ."
+    sh """
+        export NEXT_PUBLIC_API_KEY="${apiKey}"
+        docker build ${buildArgs} -t ${imageName}:${tag} .
+    """
 }
