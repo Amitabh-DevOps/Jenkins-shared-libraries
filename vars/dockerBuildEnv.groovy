3e8a8a4e1fe4d7831d805dbfab48137d229a08e1
Amitabh soni <amitabhdevops2024@gmail.com>
2025-04-04 16:31:55 +0530

    Delete vars/dockerBuildEnv.groovy

--- vars/dockerBuildEnv.groovy
+++
@@ -1,9 +0,0 @@
-// vars/dockerBuildEnv.groovy
-// vars/dockerbuild.groovy
-
-def call(String imageName, String tag, String buildArgs = "", String apiKey) {
-    sh """
-        export NEXT_PUBLIC_API_KEY="${apiKey}"
-        docker build ${buildArgs} -t ${imageName}:${tag} .
-    """
-}
